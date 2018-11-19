1efc7ff80f5942e8a2c83b10f7162bd10225a31a
Merih <merimemax@gmail.com>
2018-11-19 12:11:29 -0600

    login

--- src/main/java/edu/mum/registar/controller/studentController/LogInController.java
+++ src/main/java/edu/mum/registar/controller/studentController/LogInController.java
@@ -1,9 +1,14 @@
 package edu.mum.registar.controller.studentController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LogInController {
 
+    @GetMapping(value = "/login")
+    public String login(){
+        return "student/login";
+    }
 
 }
--- src/main/webapp/WEB-INF/Views/student/singup.jsp
+++ src/main/webapp/WEB-INF/Views/student/singup.jsp
@@ -8,9 +8,11 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>signin</title>
+    <title>sign up</title>
 </head>
 <body>
 
+
+<h1>Sign up</h1>
 </body>
 </html>
