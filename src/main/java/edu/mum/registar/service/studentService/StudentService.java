3f6dc757cb440444679d85678f1e6e25cfa880e9
Merih <merimemax@gmail.com>
2018-11-19 22:19:01 -0600

    domain relationship updates

--- src/main/java/edu/mum/registar/service/studentService/StudentService.java
+++ src/main/java/edu/mum/registar/service/studentService/StudentService.java
@@ -2,7 +2,13 @@ package edu.mum.registar.service.studentService;
 
 import edu.mum.registar.domain.Student;
 
+import java.util.List;
+
 public interface StudentService {
 
     public Student findStudentByEmail(String email);
+
+    public List<Student> findAllStudent();
+
+
 }
